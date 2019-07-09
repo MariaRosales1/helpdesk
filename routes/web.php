c2bbde232278a75a4515f0b99bbf410b2c440d26
Santiago  Quintero <saquinteros@unal.edu.co>
2019-07-09 13:43:41 -0500

    Encriptacion de contraseña

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Route::resource('timeservice', 'TimeServiceController');
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
