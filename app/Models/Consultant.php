b6dca8dd5a5526942e4a461eb94ea553562369e5
Daniel Mauricio Ceballos Gonzalez <dmceballosg@unal.edu.co>
2019-07-25 12:29:50 -0500

    Login y validaciones con logo
    lo del titulo

--- app/Models/Consultant.php
+++ app/Models/Consultant.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable; 
 
 class Consultant extends Model
 {
-    protected $guarded = [];
+    protected $guarded = ['password'];
+ 
+    public function getAuthPassword()
+    {
+        return $this->this->password;
+    }
 }
+
--- app/User.php
+++ app/User.php
@@ -10,30 +10,31 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $table = 'consultants';
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
     protected $fillable = [
-        'name', 'email', 'password',
+    'email', 'password',
     ];
-
+    
     /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
+    * The attributes that should be hidden for arrays.
+    *
+    * @var array
+    */
     protected $hidden = [
-        'password', 'remember_token',
+    'password',
     ];
-
+    
     /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
+    * The attributes that should be cast to native types.
+    *
+    * @var array
+    */
     protected $casts = [
-        'email_verified_at' => 'datetime',
+    'email_verified_at' => 'datetime',
     ];
 }
