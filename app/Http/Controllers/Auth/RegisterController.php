2ae405f6fa83d144f4ba66c92c387f519abeda26
MariaRosales1 <mirosaless@unal.edu.co>
2019-08-01 20:27:41 -0500

    mensaje de validaciones hecho

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use App\Models\Consultant;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\ConsultantController;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
@@ -28,7 +30,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/register';
 
     /**
      * Create a new controller instance.
@@ -46,13 +48,38 @@ class RegisterController extends Controller
      * @param  array  $data
      * @return \Illuminate\Contracts\Validation\Validator
      */
+    // public function register(Request $request)
+    // {
+    //     $this->validator($request->all())->validate();
+
+    //     event(new Registered($user = $this->create($request->all())));
+
+    //     //$this->guard()->login($user);
+
+    //     return $this->registered($request, $user)
+    //                     ?: redirect($this->redirectPath());
+    // }
+
     protected function validator(array $data)
     {
+
         return Validator::make($data, [
-            'identification' => 'required|min:0|integer|digits_between:5,15|unique:consultants',
+            'identification' => 'required|min:0|integer|digits_between:5,15|unique:users',
             'name' => 'required',
-            'email' => 'required|email|unique:consultants',
+            'email' => 'required|email|unique:users',
             'password' => 'required|min:6'
+        ], [
+            'identification.required' => 'La identificación es obligatoria',
+            'identification.integer' => 'La identificación debe ser numérica',
+            'digits_between'=> 'La identificación debe contener entre 5 y 15 digítos',
+            'identification.min' => 'La identificación no puede ser negativa',
+            'identification.unique' => 'La identificación ya esta en uso',
+            'name.required' => 'El nombre es obligatorio',
+            'email.required' => 'El correo electronico es obligatorio',
+            'email.email' => 'El correo electrónico es incorrecto',
+            'email.unique' => 'El correo electrónico ya está en uso',
+            'password.required' => 'La contraseña es obligatoria',
+            'password.min' => 'La contraseña debe tener al menos 6 caracteres',
         ]);
     }
 
@@ -64,11 +91,20 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        $usuario= User::create([
             'identification' => $data['identification'],
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
         ]);
+
+        Consultant::create([
+            'number_ticket' => 0,
+            'conected' => 'desconectado',
+            'user_id' => $usuario->id,
+        ]);
+
+
+        return $usuario;
     }
 }
