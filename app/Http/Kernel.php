4112d306e3ae8f6824b6f2faa41804f54823f419
Santiago  Quintero <saquinteros@unal.edu.co>
2019-08-02 21:39:14 -0500

    Permisos
    Permisos para el usuario admin y consultants, agrupado de rutas para los accesos de admin, agregada restrictición de tamaño a name

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,6 +51,8 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'permissionAdmin' => \App\Http\Middleware\PermissionAdminMiddleware::class,
+        'permissionConsultant' => \App\Http\Middleware\PermissionConsultantMiddleware::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
