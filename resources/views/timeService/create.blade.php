0f57090e162f624df0dbba03ad5b869fe5f7b1f0
MariaRosales1 <mirosaless@unal.edu.co>
2019-08-18 22:27:58 -0500

    actualizado timeservice

--- resources/views/timeService/create.blade.php
+++ resources/views/timeService/create.blade.php
@@ -20,20 +20,20 @@
             <br>
             <div class="row">
                 <div class="col">
-                    <select class="custom-select" name="diaInicio">
+                    <select class="custom-select" name="start_day">
                         @foreach(['LUNES', 'MARTES', 'MIERCOLES', 'JUEVES', 'VIERNES', 'SABADO', 'DOMINGO'] as $day)
                             <option value="{{$day}}"
-                                    @if (($timeService) && $day == $timeService->diaInicio) selected="selected" @endif>
+                                    @if (($timeService) && $day == $timeService->start_day) selected="selected" @endif>
                                 {{ucfirst(strtolower($day))}}
                             </option>
                         @endforeach
                     </select>
                 </div>
                 <div class="col">
-                    <select class="custom-select" name="diaFin">
+                    <select class="custom-select" name="final_day">
                         @foreach(['LUNES', 'MARTES', 'MIERCOLES', 'JUEVES', 'VIERNES', 'SABADO', 'DOMINGO'] as $day)
                             <option value="{{$day}}"
-                                    @if (($timeService) && $day == $timeService->diaFin) selected="selected" @endif>
+                                    @if (($timeService) && $day == $timeService->final_day) selected="selected" @endif>
                                 {{ucfirst(strtolower($day))}}
                             </option>
                         @endforeach
@@ -43,12 +43,12 @@
             <br>
             <div class="row">
                 <div class="col">
-                    <input type="time" class="form-control" placeholder="Hora Inicio" name="horaInicio"
-                           value="{{$timeService ? date('H:i', strtotime($timeService->horaInicio)) : date('H:i', strtotime('08:00:00'))}}">
+                    <input type="time" class="form-control" placeholder="Hora Inicio" name="start_time"
+                           value="{{$timeService ? date('H:i', strtotime($timeService->start_time)) : date('H:i', strtotime('08:00:00'))}}">
                 </div>
                 <div class="col">
-                    <input type="time" class="form-control" placeholder="Hora Fin" name="horaFin"
-                           value="{{$timeService ? date('H:i', strtotime($timeService->horaFin)) : date('H:i', strtotime('17:00:00'))}}">
+                    <input type="time" class="form-control" placeholder="Hora Fin" name="final_time"
+                           value="{{$timeService ? date('H:i', strtotime($timeService->final_time)) : date('H:i', strtotime('17:00:00'))}}">
                 </div>
             </div>
             <br>
