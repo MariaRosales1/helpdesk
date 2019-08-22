74dad274df2d6385939e405ed1bc1896ea974fe7
MariaRosales1 <mirosaless@unal.edu.co>
2019-08-21 22:24:57 -0500

    actualizado modelo cliente y ticket
    se creo el modelo cliente con su migración y vista según el diagrama de secuencias. Se creo tambien el modelo ticket con su migración

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,9 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </head>
 <body>
     <div id="app">
